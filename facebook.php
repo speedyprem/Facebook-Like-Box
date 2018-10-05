f74a6defe88ef40d48db999c73993c4b2d5cd9fa
Prem Tiwari <prem.tiwari@outlook.com>
2018-10-05 13:30:20 +0530

    refactor the code

--- facebook.php
+++ facebook.php
@@ -8,52 +8,29 @@ Author: Prem Tiwari
 Author URI: https://www.freewebmentor.com
 License: GPL2
 */
-$themater_facebook_defaults = array(
-    'title' => 'Facebook',
-    'url' => 'https://www.facebook.com/freewebmentor',
-    'colorscheme' => 'light',
-    'show_faces' => 'true',
-    'stream' => 'false',
-    'header' => 'false',
-    'border' => '#ffffff'
-);
 
-$theme->options['widgets_options']['facebook'] =  isset( $theme->options['widgets_options']['facebook'] )
-    ? array_merge($themater_facebook_defaults, $theme->options['widgets_options']['facebook'])
-    : $themater_facebook_defaults;
-
-
-add_action( 'widgets_init', function () {
-    return register_widget("ThematerFacebook");
-} );
-
-class ThematerFacebook extends WP_Widget
+class ResponsiveFacebookLikeBox extends WP_Widget
 {
-    function __construct()
-    {
-        parent::__construct(
-            'themater_facebook',
-            '&raquo; Facebook Like Box',
-            array( 'description' => ('Facebook Like Box social widget enables 
-            facebook page owners to attract & gain likes from their website.')
-            )
-        );
-    }
+	function __construct()
+	{
+		parent::__construct(
+			'themater_facebook',
+			'&raquo; Facebook Like Box',
+			array( 'description' => ('Facebook Like Box social widget. Enables Facebook Page owners to attract and gain Likes from their own website.')
+			));
+	}
 
-    function widget( $args, $instance )
-    {
-        global $wpdb, $theme;
-        extract( $args );
-        $title = apply_filters('widget_title', $instance['title']);
-        $url = $instance['url'];
-        $colorscheme = $instance['colorscheme'];
-        $show_faces = $instance['show_faces'] == 'true' ? 'true' : 'false';
-        $stream = $instance['stream'] == 'true' ? 'true' : 'false';
-        $header = $instance['header'] == 'true' ? 'true' : 'false';
-        $border = $instance['border'];
-        ?>
+	function widget($args, $instance)
+	{
+		$title = apply_filters('widget_title', $instance['title']);
+		$url = $instance['url'];
+		$show_faces = $instance['show_faces'] == 'true' ? 'true' : 'false';
+		$stream = $instance['stream'] == 'true' ? 'true' : 'false';
+		$header = $instance['header'] == 'true' ? 'true' : 'false';
+		$border = $instance['border'];
+		?>
 
-        <?php  if ( $title ) {  ?> <h3 class="widgettitle"><?php echo $title; ?></h3> <?php }  ?>
+		<?php  if ( $title ) {  ?> <h3 class="widgettitle"><?php echo $title; ?></h3> <?php }  ?>
         <div id="fb-root"></div>
         <script>(function(d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0];
@@ -64,61 +41,62 @@ class ThematerFacebook extends WP_Widget
             }(document, 'script', 'facebook-jssdk'));</script>
         <div class="fb-like-box" data-href="<?php echo $url; ?>" data-width="" data-height="" data-colorscheme="<?php echo $colorscheme; ?>" data-show-faces="<?php echo $show_faces; ?>" data-stream="<?php echo $stream; ?>" data-header="<?php echo $header; ?>" data-border-color="<?php echo $border; ?>"></div>
 
-        <?php
-    }
-    /**
-     * Update the $old_instance values with $new_instance in widgets
-     */
-    function update( $new_instance, $old_instance )
-    {
-        $instance = $old_instance;
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['url'] = strip_tags($new_instance['url']);
-        $instance['width'] = strip_tags($new_instance['width']);
-        $instance['height'] = strip_tags($new_instance['height']);
-        $instance['colorscheme'] = strip_tags($new_instance['colorscheme']);
-        $instance['show_faces'] = strip_tags($new_instance['show_faces']);
-        $instance['stream'] = strip_tags($new_instance['stream']);
-        $instance['header'] = strip_tags($new_instance['header']);
-        $instance['border'] = strip_tags($new_instance['border']);
-        return $instance;
-    }
+		<?php
+	}
+	function update($new_instance, $old_instance)
+	{
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['url'] = strip_tags($new_instance['url']);
+		$instance['width'] = strip_tags($new_instance['width']);
+		$instance['height'] = strip_tags($new_instance['height']);
+		$instance['colorscheme'] = strip_tags($new_instance['colorscheme']);
+		$instance['show_faces'] = strip_tags($new_instance['show_faces']);
+		$instance['stream'] = strip_tags($new_instance['stream']);
+		$instance['header'] = strip_tags($new_instance['header']);
+		$instance['border'] = strip_tags($new_instance['border']);
+		return $instance;
+	}
 
-    function form($instance)
-    {
-        global $theme;
-        $instance = wp_parse_args((array) $instance, $theme->options['widgets_options']['facebook']);
-        ?>
-        <div class="tt-widget">
-            <table width="100%">
-                <tr>
-                    <td class="tt-widget-label" width="30%"><label for="<?php echo $this->get_field_id('title'); ?>">Title:</label></td>
-                    <td class="tt-widget-content" width="70%"><input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($instance['title']); ?>" /></td>
-                </tr>
-                <tr>
-                    <td class="tt-widget-label"><label for="<?php echo $this->get_field_id('url'); ?>">Facebook Page URL:</label></td>
-                    <td class="tt-widget-content"><input class="widefat" id="<?php echo $this->get_field_id('url'); ?>" name="<?php echo $this->get_field_name('url'); ?>" type="text" value="<?php echo esc_attr($instance['url']); ?>" /></td>
-                </tr>
-                <tr> 
-                    <td class="tt-widget-label">Color Scheme:</td>
-                    <td class="tt-widget-content">
-                        <select name="<?php echo $this->get_field_name('colorscheme'); ?>">
-                            <option value="light" <?php selected('alignleft', $instance['colorscheme']); ?> >Light</option>
-                            <option value="dark"  <?php selected('alignright', $instance['colorscheme']); ?>>Dark</option>
-                        </select>
-                        &nbsp; &nbsp; Border Color: <input type="text" style="width: 50px;" name="<?php echo $this->get_field_name('border'); ?>" value="<?php echo esc_attr($instance['border']); ?>" /> <em>e.g: #ffffff</em>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="tt-widget-label">Misc Options:</td>
-                    <td class="tt-widget-content">
-                        <input type="checkbox" name="<?php echo $this->get_field_name('show_faces'); ?>"  <?php checked('true', $instance['show_faces']); ?> value="true" />  <?php _e('Show Faces', 'themater'); ?>
-                        <br /><input type="checkbox" name="<?php echo $this->get_field_name('stream'); ?>"  <?php checked('true', $instance['stream']); ?> value="true" />  <?php _e('Show Stream', 'themater'); ?>
-                        <br /><input type="checkbox" name="<?php echo $this->get_field_name('header'); ?>"  <?php checked('true', $instance['header']); ?> value="true" />  <?php _e('Show Header', 'themater'); ?>
-                    </td>
-                </tr>
-            </table>
+	function form($instance)
+	{
+		global $theme;
+		$instance = wp_parse_args( (array) $instance, $theme->options['widgets_options']['facebook'] );
+		?>
+
+        <div class="widget">
+            <p>
+                <label for="<?php echo $this->get_field_id('title'); ?>">Title: </label>
+                <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" placeholder="e.g: Follow us" value="<?php echo esc_attr($instance['title']); ?>" />
+            </p>
+            <p>
+                <label for="<?php echo $this->get_field_id('url'); ?>">Page URL:</label>
+                <input class="widefat" id="<?php echo $this->get_field_id('url'); ?>" name="<?php echo $this->get_field_name('url'); ?>" type="text" placeholder="e.g: https://www.facebook.com/freewebmentor/" value="<?php echo esc_attr($instance['url']); ?>" />
+            </p>
+            <p>
+                <b>Advance Options:</b>
+            </p>
+            <p>
+                <select name="<?php echo $this->get_field_name('colorscheme'); ?>" style="width: 80px;">
+                    <option value="light" <?php selected('alignleft', $instance['colorscheme']); ?> >Light</option>
+                    <option value="dark"  <?php selected('alignright', $instance['colorscheme']); ?>>Dark</option>
+                </select>
+                <label for="<?php echo $this->get_field_id('colorscheme'); ?>">Color Scheme</label>
+            </p>
+            <p>
+                <input type="text" style="width: 80px;" name="<?php echo $this->get_field_name('border'); ?>" placeholder="e.g: #ffffff" value="<?php echo esc_attr($instance['border']); ?>" />
+                <label for="<?php echo $this->get_field_id('colorscheme'); ?>">Border Color</label>
+            </p>
+            <p>
+                <input type="checkbox" name="<?php echo $this->get_field_name('show_faces'); ?>"  <?php checked('true', $instance['show_faces']); ?> value="true" />  <?php _e('Show Faces', 'themater'); ?>
+                <br /><input type="checkbox" name="<?php echo $this->get_field_name('stream'); ?>"  <?php checked('true', $instance['stream']); ?> value="true" />  <?php _e('Show Stream', 'themater'); ?>
+                <br /><input type="checkbox" name="<?php echo $this->get_field_name('header'); ?>"  <?php checked('true', $instance['header']); ?> value="true" />  <?php _e('Show Header', 'themater'); ?>
+            </p>
         </div>
-        <?php
-    }
+		<?php
+	}
 }
+
+add_action( 'widgets_init', function() {
+	return register_widget("ResponsiveFacebookLikeBox");
+} );
