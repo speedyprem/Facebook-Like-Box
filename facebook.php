338e2aabdff156a7fd95714f18565d8f85f284a7
Prem Tiwari <prem.tiwari@news.co.uk>
2019-07-15 15:26:15 +0530

    Sync code with the WordPress.org plugin directory

--- facebook.php
+++ facebook.php
@@ -1,18 +1,13 @@
 <?php
 /*
-Plugin Name: Responsive Facebook Like Box
+Plugin Name: Responsive Like Box
 Plugin URI: https://www.freewebmentor.com/2013/10/facebook-like-box-wordpress.html
 Description: This plugin helps you create a simple widgets for facebook like box in WordPress.
-Version: 3.0
+Version: 2.2.7
 Author: Prem Tiwari
 Author URI: https://www.freewebmentor.com
 License: GPL2
 */
-/**
- * BLOCK: Facebook Like Box.
- */
-require_once( plugin_dir_path( __FILE__ ) . '/inc/block.php' );
-
 class ResponsiveFacebookLikeBox extends WP_Widget
 {
 	function __construct()
@@ -35,15 +30,15 @@ class ResponsiveFacebookLikeBox extends WP_Widget
 		?>
 
 		<?php  if ( $title ) {  ?> <h3 class="widgettitle"><?php echo $title; ?></h3> <?php }  ?>
-        <div id="fb-root"></div>
-        <script>(function(d, s, id) {
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) {return;}
                 js = d.createElement(s); js.id = id;
                 js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
                 fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));</script>
-        <div class="fb-like-box" data-href="<?php echo $url; ?>" data-width="" data-height="" data-colorscheme="<?php echo $colorscheme; ?>" data-show-faces="<?php echo $show_faces; ?>" data-stream="<?php echo $stream; ?>" data-header="<?php echo $header; ?>" data-border-color="<?php echo $border; ?>"></div>
+		<div class="fb-like-box" data-href="<?php echo $url; ?>" data-width="" data-height="" data-colorscheme="<?php echo $colorscheme; ?>" data-show-faces="<?php echo $show_faces; ?>" data-stream="<?php echo $stream; ?>" data-header="<?php echo $header; ?>" data-border-color="<?php echo $border; ?>"></div>
 
 		<?php
 	}
@@ -68,35 +63,35 @@ class ResponsiveFacebookLikeBox extends WP_Widget
 		$instance = wp_parse_args( (array) $instance, $theme->options['widgets_options']['facebook'] );
 		?>
 
-        <div class="widget">
-            <p>
-                <label for="<?php echo $this->get_field_id('title'); ?>">Title: </label>
-                <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" placeholder="e.g: Follow us" value="<?php echo esc_attr($instance['title']); ?>" />
-            </p>
-            <p>
-                <label for="<?php echo $this->get_field_id('url'); ?>">Page URL:</label>
-                <input class="widefat" id="<?php echo $this->get_field_id('url'); ?>" name="<?php echo $this->get_field_name('url'); ?>" type="text" placeholder="e.g: https://www.facebook.com/freewebmentor/" value="<?php echo esc_attr($instance['url']); ?>" />
-            </p>
-            <p>
-                <b>Advance Options:</b>
-            </p>
-            <p>
-                <select name="<?php echo $this->get_field_name('colorscheme'); ?>" style="width: 80px;">
-                    <option value="light" <?php selected('alignleft', $instance['colorscheme']); ?> >Light</option>
-                    <option value="dark"  <?php selected('alignright', $instance['colorscheme']); ?>>Dark</option>
-                </select>
-                <label for="<?php echo $this->get_field_id('colorscheme'); ?>">Color Scheme</label>
-            </p>
-            <p>
-                <input type="text" style="width: 80px;" name="<?php echo $this->get_field_name('border'); ?>" placeholder="e.g: #ffffff" value="<?php echo esc_attr($instance['border']); ?>" />
-                <label for="<?php echo $this->get_field_id('colorscheme'); ?>">Border Color</label>
-            </p>
-            <p>
-                <input type="checkbox" name="<?php echo $this->get_field_name('show_faces'); ?>"  <?php checked('true', $instance['show_faces']); ?> value="true" />  <?php _e('Show Faces', 'themater'); ?>
-                <br /><input type="checkbox" name="<?php echo $this->get_field_name('stream'); ?>"  <?php checked('true', $instance['stream']); ?> value="true" />  <?php _e('Show Stream', 'themater'); ?>
-                <br /><input type="checkbox" name="<?php echo $this->get_field_name('header'); ?>"  <?php checked('true', $instance['header']); ?> value="true" />  <?php _e('Show Header', 'themater'); ?>
-            </p>
-        </div>
+		<div class="widget">
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>">Title: </label>
+				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" placeholder="e.g: Follow us" value="<?php echo esc_attr($instance['title']); ?>" />
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id('url'); ?>">Page URL:</label>
+				<input class="widefat" id="<?php echo $this->get_field_id('url'); ?>" name="<?php echo $this->get_field_name('url'); ?>" type="text" placeholder="e.g: https://www.facebook.com/freewebmentor/" value="<?php echo esc_attr($instance['url']); ?>" />
+			</p>
+			<p>
+				<b>Advance Options:</b>
+			</p>
+			<p>
+				<select name="<?php echo $this->get_field_name('colorscheme'); ?>" style="width: 80px;">
+					<option value="light" <?php selected('alignleft', $instance['colorscheme']); ?> >Light</option>
+					<option value="dark"  <?php selected('alignright', $instance['colorscheme']); ?>>Dark</option>
+				</select>
+				<label for="<?php echo $this->get_field_id('colorscheme'); ?>">Color Scheme</label>
+			</p>
+			<p>
+				<input type="text" style="width: 80px;" name="<?php echo $this->get_field_name('border'); ?>" placeholder="e.g: #ffffff" value="<?php echo esc_attr($instance['border']); ?>" />
+				<label for="<?php echo $this->get_field_id('colorscheme'); ?>">Border Color</label>
+			</p>
+			<p>
+				<input type="checkbox" name="<?php echo $this->get_field_name('show_faces'); ?>"  <?php checked('true', $instance['show_faces']); ?> value="true" />  <?php _e('Show Faces', 'themater'); ?>
+				<br /><input type="checkbox" name="<?php echo $this->get_field_name('stream'); ?>"  <?php checked('true', $instance['stream']); ?> value="true" />  <?php _e('Show Stream', 'themater'); ?>
+				<br /><input type="checkbox" name="<?php echo $this->get_field_name('header'); ?>"  <?php checked('true', $instance['header']); ?> value="true" />  <?php _e('Show Header', 'themater'); ?>
+			</p>
+		</div>
 		<?php
 	}
 }
