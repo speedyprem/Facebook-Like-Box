7e2eeab2780e8d0efc458fcc07233b3d56ee8627
Tiwari <ptiwari@192.168.1.3>
2019-10-24 01:34:21 +0530

    Code forming

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -16,25 +16,26 @@ class RflbShortcode {
 	public function rflb_shortcode() {
 		if ( is_singular() ) {
 			$widget_themater_facebook = get_option( 'widget_themater_facebook', true );
-		?>
-		<div id="fb-root"></div>
-		<script>(function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) {
-                    return;
-                }
-                js = d.createElement(s);
-                js.id = id;
-                js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
-		<div class="fb-like-box" data-href="<?php echo $widget_themater_facebook[2]['url']; ?>"
-			 data-show-faces="<?php echo $widget_themater_facebook[2]['show_faces']; ?>"
-			 data-stream="<?php echo $widget_themater_facebook[2]['stream']; ?>"
-			 data-colorscheme="<?php echo $widget_themater_facebook[2]['colorscheme']; ?>"
-			 data-header="<?php echo $widget_themater_facebook[2]['header']; ?>">
-		</div>
-	<?php } }
+			?>
+            <div id="fb-root"></div>
+            <script>(function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) {
+                        return;
+                    }
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
+            <div class="fb-like-box" data-href="<?php echo $widget_themater_facebook[ 2 ][ 'url' ]; ?>"
+                 data-show-faces="<?php echo $widget_themater_facebook[ 2 ][ 'show_faces' ]; ?>"
+                 data-stream="<?php echo $widget_themater_facebook[ 2 ][ 'stream' ]; ?>"
+                 data-colorscheme="<?php echo $widget_themater_facebook[ 2 ][ 'colorscheme' ]; ?>"
+                 data-header="<?php echo $widget_themater_facebook[ 2 ][ 'header' ]; ?>">
+            </div>
+		<?php }
+	}
 
 }
 
